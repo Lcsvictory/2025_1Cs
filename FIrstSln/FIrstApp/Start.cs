134aa5b736e2505e559898bf52e0a066c95fcac2
Lcsvictory <47920707+Lcsvictory@users.noreply.github.com>
2025-03-05 15:48:53 +0900

    week1 OT

---
+++ FIrstSln/FIrstApp/Start.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIrstApp
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Lee");
+            int a = 10;
+            int b = 20;
+            Console.Write(a + b);
+        }
+    }
+}
