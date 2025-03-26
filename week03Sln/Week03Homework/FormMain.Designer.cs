3f10501713d6cde0f3e489ae358317b989a8e35a
Lcsvictory <yuko135z@gmail.com>
2025-03-27 08:47:47 +0900

    week03 calculator infix to postfix & todo .연산

--- week03Sln/Week03Homework/FormMain.Designer.cs
+++ week03Sln/Week03Homework/FormMain.Designer.cs
@@ -45,31 +45,40 @@
             this.btnNum0 = new System.Windows.Forms.Button();
             this.btnCal = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblExpression
             // 
-            this.lblExpression.Location = new System.Drawing.Point(13, 19);
+            this.lblExpression.Location = new System.Drawing.Point(18, 28);
+            this.lblExpression.Margin = new System.Windows.Forms.Padding(4);
             this.lblExpression.Name = "lblExpression";
-            this.lblExpression.Size = new System.Drawing.Size(285, 41);
+            this.lblExpression.Size = new System.Drawing.Size(407, 62);
             this.lblExpression.TabIndex = 0;
+            this.lblExpression.Tag = "";
             // 
             // lblNumbers
             // 
             this.lblNumbers.BackColor = System.Drawing.Color.White;
             this.lblNumbers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblNumbers.Location = new System.Drawing.Point(12, 72);
+            this.lblNumbers.Location = new System.Drawing.Point(17, 108);
+            this.lblNumbers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNumbers.Name = "lblNumbers";
-            this.lblNumbers.Size = new System.Drawing.Size(286, 40);
+            this.lblNumbers.Size = new System.Drawing.Size(408, 59);
             this.lblNumbers.TabIndex = 1;
             this.lblNumbers.Text = "0";
             this.lblNumbers.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // btnNum7
             // 
-            this.btnNum7.Location = new System.Drawing.Point(16, 126);
+            this.btnNum7.Location = new System.Drawing.Point(23, 270);
+            this.btnNum7.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum7.Name = "btnNum7";
-            this.btnNum7.Size = new System.Drawing.Size(66, 48);
+            this.btnNum7.Size = new System.Drawing.Size(94, 72);
             this.btnNum7.TabIndex = 2;
             this.btnNum7.Text = "7";
             this.btnNum7.UseVisualStyleBackColor = true;
@@ -77,9 +86,10 @@
             // 
             // btnNum8
             // 
-            this.btnNum8.Location = new System.Drawing.Point(88, 126);
+            this.btnNum8.Location = new System.Drawing.Point(126, 270);
+            this.btnNum8.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum8.Name = "btnNum8";
-            this.btnNum8.Size = new System.Drawing.Size(66, 48);
+            this.btnNum8.Size = new System.Drawing.Size(94, 72);
             this.btnNum8.TabIndex = 2;
             this.btnNum8.Text = "8";
             this.btnNum8.UseVisualStyleBackColor = true;
@@ -87,9 +97,10 @@
             // 
             // btnNum9
             // 
-            this.btnNum9.Location = new System.Drawing.Point(160, 126);
+            this.btnNum9.Location = new System.Drawing.Point(229, 270);
+            this.btnNum9.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum9.Name = "btnNum9";
-            this.btnNum9.Size = new System.Drawing.Size(66, 48);
+            this.btnNum9.Size = new System.Drawing.Size(94, 72);
             this.btnNum9.TabIndex = 2;
             this.btnNum9.Text = "9";
             this.btnNum9.UseVisualStyleBackColor = true;
@@ -97,18 +108,21 @@
             // 
             // btnDiv
             // 
-            this.btnDiv.Location = new System.Drawing.Point(232, 126);
+            this.btnDiv.Location = new System.Drawing.Point(331, 189);
+            this.btnDiv.Margin = new System.Windows.Forms.Padding(4);
             this.btnDiv.Name = "btnDiv";
-            this.btnDiv.Size = new System.Drawing.Size(66, 48);
+            this.btnDiv.Size = new System.Drawing.Size(94, 72);
             this.btnDiv.TabIndex = 2;
             this.btnDiv.Text = "/";
             this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnOperater_Click);
             // 
             // btnNum4
             // 
-            this.btnNum4.Location = new System.Drawing.Point(16, 180);
+            this.btnNum4.Location = new System.Drawing.Point(23, 351);
+            this.btnNum4.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum4.Name = "btnNum4";
-            this.btnNum4.Size = new System.Drawing.Size(66, 48);
+            this.btnNum4.Size = new System.Drawing.Size(94, 72);
             this.btnNum4.TabIndex = 2;
             this.btnNum4.Text = "4";
             this.btnNum4.UseVisualStyleBackColor = true;
@@ -116,9 +130,10 @@
             // 
             // btnNum5
             // 
-            this.btnNum5.Location = new System.Drawing.Point(88, 180);
+            this.btnNum5.Location = new System.Drawing.Point(126, 351);
+            this.btnNum5.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum5.Name = "btnNum5";
-            this.btnNum5.Size = new System.Drawing.Size(66, 48);
+            this.btnNum5.Size = new System.Drawing.Size(94, 72);
             this.btnNum5.TabIndex = 2;
             this.btnNum5.Text = "5";
             this.btnNum5.UseVisualStyleBackColor = true;
@@ -126,9 +141,10 @@
             // 
             // btnNum6
             // 
-            this.btnNum6.Location = new System.Drawing.Point(160, 180);
+            this.btnNum6.Location = new System.Drawing.Point(229, 351);
+            this.btnNum6.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum6.Name = "btnNum6";
-            this.btnNum6.Size = new System.Drawing.Size(66, 48);
+            this.btnNum6.Size = new System.Drawing.Size(94, 72);
             this.btnNum6.TabIndex = 2;
             this.btnNum6.Text = "6";
             this.btnNum6.UseVisualStyleBackColor = true;
@@ -136,18 +152,21 @@
             // 
             // btnMul
             // 
-            this.btnMul.Location = new System.Drawing.Point(232, 180);
+            this.btnMul.Location = new System.Drawing.Point(331, 270);
+            this.btnMul.Margin = new System.Windows.Forms.Padding(4);
             this.btnMul.Name = "btnMul";
-            this.btnMul.Size = new System.Drawing.Size(66, 48);
+            this.btnMul.Size = new System.Drawing.Size(94, 72);
             this.btnMul.TabIndex = 2;
             this.btnMul.Text = "*";
             this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnOperater_Click);
             // 
             // btnNum1
             // 
-            this.btnNum1.Location = new System.Drawing.Point(16, 234);
+            this.btnNum1.Location = new System.Drawing.Point(23, 432);
+            this.btnNum1.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum1.Name = "btnNum1";
-            this.btnNum1.Size = new System.Drawing.Size(66, 48);
+            this.btnNum1.Size = new System.Drawing.Size(94, 72);
             this.btnNum1.TabIndex = 2;
             this.btnNum1.Text = "1";
             this.btnNum1.UseVisualStyleBackColor = true;
@@ -155,9 +174,10 @@
             // 
             // btnNum2
             // 
-            this.btnNum2.Location = new System.Drawing.Point(88, 234);
+            this.btnNum2.Location = new System.Drawing.Point(126, 432);
+            this.btnNum2.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum2.Name = "btnNum2";
-            this.btnNum2.Size = new System.Drawing.Size(66, 48);
+            this.btnNum2.Size = new System.Drawing.Size(94, 72);
             this.btnNum2.TabIndex = 2;
             this.btnNum2.Text = "2";
             this.btnNum2.UseVisualStyleBackColor = true;
@@ -165,9 +185,10 @@
             // 
             // btnNum3
             // 
-            this.btnNum3.Location = new System.Drawing.Point(160, 234);
+            this.btnNum3.Location = new System.Drawing.Point(229, 432);
+            this.btnNum3.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum3.Name = "btnNum3";
-            this.btnNum3.Size = new System.Drawing.Size(66, 48);
+            this.btnNum3.Size = new System.Drawing.Size(94, 72);
             this.btnNum3.TabIndex = 2;
             this.btnNum3.Text = "3";
             this.btnNum3.UseVisualStyleBackColor = true;
@@ -175,18 +196,21 @@
             // 
             // btnSub
             // 
-            this.btnSub.Location = new System.Drawing.Point(232, 234);
+            this.btnSub.Location = new System.Drawing.Point(331, 351);
+            this.btnSub.Margin = new System.Windows.Forms.Padding(4);
             this.btnSub.Name = "btnSub";
-            this.btnSub.Size = new System.Drawing.Size(66, 48);
+            this.btnSub.Size = new System.Drawing.Size(94, 72);
             this.btnSub.TabIndex = 2;
             this.btnSub.Text = "-";
             this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnOperater_Click);
             // 
             // btnNum0
             // 
-            this.btnNum0.Location = new System.Drawing.Point(16, 288);
+            this.btnNum0.Location = new System.Drawing.Point(23, 513);
+            this.btnNum0.Margin = new System.Windows.Forms.Padding(4);
             this.btnNum0.Name = "btnNum0";
-            this.btnNum0.Size = new System.Drawing.Size(66, 48);
+            this.btnNum0.Size = new System.Drawing.Size(94, 72);
             this.btnNum0.TabIndex = 2;
             this.btnNum0.Text = "0";
             this.btnNum0.UseVisualStyleBackColor = true;
@@ -194,27 +218,84 @@
             // 
             // btnCal
             // 
-            this.btnCal.Location = new System.Drawing.Point(88, 288);
+            this.btnCal.Location = new System.Drawing.Point(229, 513);
+            this.btnCal.Margin = new System.Windows.Forms.Padding(4);
             this.btnCal.Name = "btnCal";
-            this.btnCal.Size = new System.Drawing.Size(138, 48);
+            this.btnCal.Size = new System.Drawing.Size(196, 72);
             this.btnCal.TabIndex = 2;
             this.btnCal.Text = "=";
             this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(232, 288);
+            this.btnAdd.Location = new System.Drawing.Point(331, 432);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(66, 48);
+            this.btnAdd.Size = new System.Drawing.Size(94, 72);
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "+";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnOperater_Click);
+            // 
+            // btnDot
+            // 
+            this.btnDot.Location = new System.Drawing.Point(125, 513);
+            this.btnDot.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(94, 72);
+            this.btnDot.TabIndex = 2;
+            this.btnDot.Text = ".";
+            this.btnDot.UseVisualStyleBackColor = true;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(23, 190);
+            this.btnLeft.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(94, 72);
+            this.btnLeft.TabIndex = 2;
+            this.btnLeft.Text = "(";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnParen_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(125, 190);
+            this.btnRight.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(94, 72);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.Text = ")";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnParen_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(227, 189);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 72);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.BackColor = System.Drawing.Color.White;
+            this.lblTest.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTest.Location = new System.Drawing.Point(548, 77);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(354, 215);
+            this.lblTest.TabIndex = 3;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(317, 358);
+            this.ClientSize = new System.Drawing.Size(975, 609);
+            this.Controls.Add(this.lblTest);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnSub);
             this.Controls.Add(this.btnNum3);
@@ -224,6 +305,10 @@
             this.Controls.Add(this.btnDiv);
             this.Controls.Add(this.btnNum5);
             this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnDot);
             this.Controls.Add(this.btnNum0);
             this.Controls.Add(this.btnNum1);
             this.Controls.Add(this.btnNum9);
@@ -232,6 +317,7 @@
             this.Controls.Add(this.btnNum7);
             this.Controls.Add(this.lblNumbers);
             this.Controls.Add(this.lblExpression);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormMain";
             this.Text = "계산기";
             this.ResumeLayout(false);
@@ -257,6 +343,11 @@
         private System.Windows.Forms.Button btnNum0;
         private System.Windows.Forms.Button btnCal;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblTest;
     }
 }
 
--- week03Sln/Week03Homework/FormMain.cs
+++ week03Sln/Week03Homework/FormMain.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,23 +13,295 @@ namespace Week03Homework
 {
     public partial class FormMain: Form
     {
+        List<string> expressionFix = new List<string>();
+        bool isOperator = false;
+        bool middleCal = false;
+        int paranCnt = 0;
+
         public FormMain()
         {
             InitializeComponent();
+
+        }
+
+        private List<string> infixToPostfix(List<string> infix)
+        {
+
+            List<string> postfix = new List<string>(); //후위표기식
+            Stack<string> stack = new Stack<string>(); //후위표기식을 생성하기위한 임시공간 == stack
+            foreach (var v in infix)
+            {
+                if (v == "(") //여는괄호는 어느 연산자보다 가장 우선순위가 높으니 일단 무조건 담음.
+                {
+                    stack.Push(v);
+                }
+                else if (v == ")") //닫는괄호면 여는괄호 나올때까지 stack에서 pop함.
+                {
+                    while (stack.Peek() != "(") //여는괄호면 false임으로 while문 탈출
+                    {
+                        postfix.Add(stack.Pop());
+                    }
+                    stack.Pop(); //현 위치는 여는괄호임. 여는괄호는 제거함.
+                }
+                else if (v == "*" || v == "/"){
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(v);
+                        continue;
+                    }
+                    else if (stack.Peek() == "(")
+                    {
+                        stack.Push(v);
+                        continue;
+                    }
+                    else
+                    {
+                        string top;
+                        while (stack.Count > 0 && ((top = stack.Peek()) == "*" || top == "/")) //top이 * 혹은 / 일때까지.
+                        {
+                            postfix.Add(stack.Pop());
+                        }
+                        stack.Push(v);
+                    }
+                }
+                else if (v == "+" || v == "-")
+                {
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(v);
+                        continue;
+                    }
+                    else if (stack.Peek() == "(")
+                    {
+                        stack.Push(v);
+                        continue;
+                    } else
+                    {
+                        string top;
+                        while (stack.Count > 0 && ((top = stack.Peek()) == "*" || top == "/" || top == "+" || top == "-")) //자기보다 우선순위가 높은것을 제낀다.
+                        {
+                            postfix.Add(stack.Pop());
+                        }
+                        stack.Push(v);
+                    }
+                } else //숫자일때. 연산자가 아닐때임.
+                {
+                    postfix.Add(v);
+                }
+
+
+            }
+            while (stack.Count != 0)
+            {
+                postfix.Add(stack.Pop());
+            }
+            
+            return postfix;
+        }
+
+        private string listTostring(List<string> postfix)
+        {
+            string result = "";
+            foreach (var i in postfix)
+            {
+                result += i;
+            }
+            return result;
+        }
+
+        private double resultCalculator(List<string> postfix)
+        {
+            //lblTest.Text = postfix.ToString();
+            double result = 0; //결과 임시저장
+            int i = 0;
+            //int a1 = 0, a2 = 0;
+            while (postfix.Count != 1)
+            {
+                switch (postfix[i])
+                {
+                    case "+":
+                        result = double.Parse(postfix[i-2]) + double.Parse(postfix[i-1]);
+                        postfix.RemoveRange(i-2, 3);
+                        postfix.Insert(i-2, result.ToString());
+                        i -= 2;
+                        break;
+                    case "-":
+                        result = double.Parse(postfix[i-2]) - double.Parse(postfix[i-1]);
+                        postfix.RemoveRange(i - 2, 3);
+                        postfix.Insert(i - 2, result.ToString());
+                        i -= 2;
+                        break;
+                    case "*":
+                        result = double.Parse(postfix[i-2]) * double.Parse(postfix[i-1]);
+                        postfix.RemoveRange(i - 2, 3);
+                        postfix.Insert(i - 2, result.ToString());
+                        i -= 2;
+                        break;
+                    case "/":
+                        result = double.Parse(postfix[i-2]) / double.Parse(postfix[i-1]);
+                        postfix.RemoveRange(i - 2, 3);
+                        postfix.Insert(i - 2, result.ToString());
+                        i -= 2;
+                        break;
+                    default:
+                        i++;
+                        continue;
+                }
+                
+            }
+            
+            return double.Parse(postfix[0]);
+        }
+
+        private List<string> testCaseReturn()
+        {
+            List<string> testPostfix = new List<string>() { "74", "+", "53", "/", "91", "*", "62", "*", "(", "66", "+", "12", ")" };
+            //foreach (var item in "A+B/C*D*(E+F)")
+            //{
+            //    testPostfix.Add(item.ToString());
+            //}
+            return testPostfix;
+        }
+
+        private void btnOperater_Click(object sender, EventArgs e)
+        {
+            
+            
+            Button target = (Button)sender;
+            if (isOperator)
+            {
+                return;
+            }
+            if (false == string.IsNullOrEmpty(lblNumbers.Text))
+            {
+
+               
+
+                if (!middleCal || lblTest.Text[lblTest.Text.Length -1] != ')')
+                {
+                    lblTest.Text += lblNumbers.Text;
+                    expressionFix.Add(lblNumbers.Text);
+                }
+
+                lblTest.Text += target.Text;
+
+
+                // 디버깅을 위해 expressionFix 리스트의 상태를 출력
+                Console.WriteLine("오퍼레이터 눌렀을때: " + string.Join(" ", expressionFix));
+
+                if (middleCal && (expressionFix[expressionFix.Count - 1] == ")" || expressionFix[expressionFix.Count - 1].All(char.IsDigit))) { 
+
+                    if (paranCnt > 0 && expressionFix[expressionFix.Count - 1] == ")")
+                    {
+                        lblNumbers.Text = resultCalculator(infixToPostfix(expressionFix)).ToString();
+                    } 
+                       
+                }
+                expressionFix.Add(target.Text);
+
+                Console.WriteLine("연산자 클릭시 " + lblTest.Text);
+                isOperator = true;
+            }
+
+            if (middleCal == false) { middleCal = true; }
+
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            if (false == string.IsNullOrEmpty(lblTest.Text) && false == isOperator && lblTest.Text.Length != 1)
+            {
+                Console.WriteLine("계산버튼시 lblTest.Text: " + lblTest.Text);
+
+                
+                if (lblTest.Text[lblTest.Text.Length -1] != ')')
+                {
+                    lblTest.Text += lblNumbers.Text;
+                    expressionFix.Add(lblNumbers.Text);
+                }
+                    //lblTest.Text += lblNumbers.Text; //숫자임
+                //expressionFix.Add(lblNumbers.Text);
+                
+               
+                if ((expressionFix[expressionFix.Count - 1] == ")" || expressionFix[expressionFix.Count - 1].All(char.IsDigit)))
+                {
+                    if (paranCnt > 0)
+                    {
+                        while (paranCnt != 0)
+                        {
+                            expressionFix.Add(")");
+                            paranCnt--;
+
+                            lblTest.Text += ")";
+                        }
+
+                    }
+                    // 디버깅을 위해 expressionFix 리스트의 상태를 출력
+                    Console.WriteLine("계산버튼 눌렀을때: " + string.Join(" ", expressionFix));
+                    lblNumbers.Text = $"{resultCalculator(infixToPostfix(expressionFix)):F2}";
+                }
+                
+                lblTest.Text += " = ";
+                lblTest.Text += lblNumbers.Text;
+
+
+            } else
+            {
+                return;
+            }
         }
 
         private void btnNumber_Click(object sender, EventArgs e)
         {
             Button target = (Button)sender;
-            if (lblNumbers.Text == "0")
+            if (lblNumbers.Text == "0" || isOperator)
             {
                 lblNumbers.Text = target.Text;
-            }else
+            }
+            else
             {
                 lblNumbers.Text += target.Text;
-            }
+                //middleCal = true;
 
+            }
+            //lblNumbers.Text = target.Text;
+            isOperator = false;
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblNumbers.Text = "0";
+            lblTest.Text = "";
+            middleCal = false;
+            isOperator = false;
+            expressionFix.Clear();
+        }
+
+        private void btnParen_Click(object sender, EventArgs e)
+        {
+            Button target = (Button)sender;
+            if (target.Text == "(")
+            {
+                lblTest.Text += "(";
+                expressionFix.Add("(");
+                paranCnt++;
+
+            }
+            else
+            {
+                if (lblNumbers.Text.Length > 0)
+                {
+                    lblTest.Text += lblNumbers.Text;
+                    lblTest.Text += ")";
+                    expressionFix.Add(lblNumbers.Text);
+                    expressionFix.Add(")");
+                    paranCnt--;
+
+                }
                 
+            }
         }
     }
 }
