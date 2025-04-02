d8bd23336c7c92e732e1718428d1ab3ab23fb4d0
Lcsvictory <yuko135z@gmail.com>
2025-04-02 16:17:46 +0900

    class들 추가

---
+++ week04Sln/Week04Homework/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04Homework
+{
+    class Department : Object
+    {
+        public string Code;
+        public string Name;
+
+        //public Department(string code, string name)
+        //{
+        //    this.Code = code;
+        //    this.Name = name;
+        //}
+
+        public override string ToString()
+        {
+            return $"[{this.Code}] {this.Name}";
+        }
+    }
+}
