7c7e8d4e4c4c4f2395630fea0f20b4c2eae9109c
Lcsvictory <yuko135z@gmail.com>
2025-04-30 15:56:18 +0900

    week09 & 과제파일

---
+++ week04_InClass/Week04Homework/Department.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04Homework
+{
+    //Object 클래스를 상속한 Department
+    class Department : object
+    {
+        private string _code;
+        private string _name;
+
+        //없으면 자동생성
+        public Department(string code, string name)
+        {
+            this._code = code;
+            this._name = name;
+        }
+
+
+        //property 속성
+        //getter+setter 한번에 구현
+        public string Code
+        {
+            get { return this._code; }
+            private set {
+                if (false == string.IsNullOrEmpty(value))
+                {
+                    this._code = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return this._name;  }
+            private set
+            {
+                if (false == string.IsNullOrEmpty(value) && value.Length >= 2)
+                {
+                    this._name = value;
+                }
+            }
+        }
+
+        public string GetCode()
+        {
+            return this._code;
+        }
+        private void SetCode(string code)
+        {
+            if (false == string.IsNullOrEmpty(code)){
+                this._code = code;
+            }
+        }
+
+
+        //python의 __str__()와 동일
+        public override string ToString()
+        {
+            return $"[{Code}] {Name}";
+        }
+    }
+}
---
+++ week04_InClass/Week04Homework/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04Homework
+{
+
+    enum YEAR
+    {
+        ONE ,
+        TWO ,
+        THREE ,
+        FOUR ,
+
+        END, // END = 4
+    }
+
+    enum CLASS
+    {
+        A,
+        B,
+        C,
+
+        END,
+    }
+    
+
+    class Student {
+
+
+        public Student(string number, string name)
+        {
+            //property로 필드에 접근하고있다.
+            this.Number = number;
+            this.Name = name;
+        }
+
+        public static Dictionary<YEAR, string> YearNames = new Dictionary<YEAR, string>()
+        {
+            {YEAR.ONE, "1학년"}, 
+            {YEAR.TWO, "2학년"},
+            {YEAR.THREE, "3학년"},
+            {YEAR.FOUR, "4학년 (심화)"},
+        };
+        
+        public string Number { get; private set; } //학번
+        
+        public string Name { get; private set; } //이름
+        public DateTime BirthInfo { get; private set; } //생년월일
+        public void setBirthInfo (int year, int month, int day)
+        {
+            BirthInfo = new DateTime(year, month, day);
+        }
+
+
+        public string DepartmentCode { get; set; } //학과코드
+        public string AdvisorNumber { get; set; } //지도교수번호
+        public YEAR Year { get; set; } // 학년
+        public CLASS Class { get; set; } // 반
+        public string RegStatus { get; set; } //재적상태
+        public string Address { get; set; } //주소
+
+        private string _contact;//연락처 
+        public string Contact
+        {
+            get { return _contact; }
+            set { this._contact = value; }
+        }
+        public override string ToString() {
+            return $"[{this.Number}]{this.Name}";
+        }
+    }
+}
